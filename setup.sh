48f6b842a09c85c3d919023c7f8a7ed416633dce
thing-a-verse <michael@wonkie.net>
2021-11-14 14:51:25 +1100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,26 +1,36 @@
-#!/bin/bash
+#!/usr/bin/bash
 
+LOG=/home/ec2-user/phase2.log
 
 function main() {
-    sudo yum install httpd -y                  >> $${LOG}
-	sudo yum install mod_wsgi -y               >> $${LOG}
-	# Tricky to log this
+    sudo yum install httpd -y                  
+	sudo yum install mod_wsgi -y 
+	
+
+	# Add mod_wsgi.so to the Apache config
 	sudo echo "LoadModule wsgi_module modules/mod_wsgi.so" >> /etc/httpd/conf/httpd.conf
+	
 	# System Manager - But you need a whole pile of policy crap - not bothering for now
 	# https://docs.aws.amazon.com/systems-manager/latest/userguide/systems-manager-ug.pdf#systems-manager-quick-setup
 	#sudo yum install -y https://s3.ap-southeast-2.amazonaws.com/amazon-ssm-ap-southeast-2/latest/linux_amd64/amazon-ssm-agent.rpm -y
 	#
-	sudo yum install git -y                    >> $${LOG}
+	# Git is already installed, else how did we get here?
+	# sudo yum install git -y   
+
+	
 	# get CTFd
 	
-	export APPDIR=/home/ec2-user/app           >> $${LOG}
-	mkdir -p $${APPDIR}                        >> $${LOG}
-	git clone https://github.com/CTFd/CTFd.git >> $${LOG}
+	export APPDIR=/home/ec2-user/app           
+	mkdir -p ${APPDIR}                        
+	git clone https://github.com/CTFd/CTFd.git 
+	
 	# Create a random key
-	cd CTFd                                    >> $${LOG}
-	head -c 64 /dev/urandom > .ctfd_secret_key >> $${LOG}
+	cd CTFd                                    
+	head -c 64 /dev/urandom > .ctfd_secret_key 
 	
     #sudo systemctl enable httpd >> $${LOG}
     #sudo systemctl start httpd >> $${LOG}
     #echo "<html><body><div>Hello, world!</div></body></html>" > /var/www/html/index.html
 }
+
+main 2>&1 > ${LOG}
