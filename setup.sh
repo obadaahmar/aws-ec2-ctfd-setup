976629a034953d44612b043f78bff9588d00e46f
thing-a-verse <michael@wonkie.net>
2021-11-14 19:19:03 +1100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,15 +15,52 @@ function root_pre() {
 	logger -s "Add mod_wsgi.so to the Apache config"
 
 	sudo echo "LoadModule wsgi_module modules/mod_wsgi.so" >> /etc/httpd/conf.d/wsgi.conf
-	
+	cd html
+	logger -s "Installing mysql"
+	sudo yum install mysql -y
 }
 
 function root_post() {
 	# Steps to run as root after main
 	#dummy homepage
 	
-	echo "<html><body><div>Hello, world\!</div></body></html>" > /var/www/html/index.html
+	echo "<html><body><div>Hello, world</div></body></html>" > /var/www/html/index.html
 	
+	cat <<EOF > /var/www/html/ctf.wsgi
+import sys
+sys.path.insert(0, '/var/www/html')
+
+from CTFd import create_app
+application = create_app()
+
+EOF
+	cat <<EOF > /etc/httpd/conf.d/ctfd.conf
+<VirtualHost *:80>
+    ServerName ctfd.wonkie.cloud
+    ServerAlias www.myserver.com
+    DocumentRoot /var/www/html
+
+    WSGIScriptAlias / /var/www/html/ctf.wsgi
+    WSGIDaemonProcess ctfd user=ctfd group=ctfd threads=5
+    WSGIProcessGroup ctfd
+
+    ErrorLog ${APACHE_LOG_DIR}/error.log
+    CustomLog ${APACHE_LOG_DIR}/access.log combined
+
+
+    #Alias /static/ /var/www/FLASKAPPS/helloworldapp/static
+    <Directory /var/www/html/CTFd>
+        Order allow,deny
+        Allow from all
+    </Directory>
+</VirtualHost>
+
+EOF
+	
+	
+	#cd /var/www/html/
+	ln -s /home/ctfd/app/CTFd/ /var/www/html/CTFd
+
 		
 	logger -s "Enable httpd"
 	sudo systemctl enable httpd
