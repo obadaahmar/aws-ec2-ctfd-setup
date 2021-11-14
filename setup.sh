01bdd7b564eb788656564b96b3d136017dab4fa6
thing-a-verse <michael@wonkie.net>
2021-11-14 18:29:08 +1100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,49 +1,73 @@
 #!/usr/bin/bash
 
 
-function main() {
+function root_pre() {
+	# Steps to run as root prior to main
 	logger -s "Installing Apache"
 	sudo yum install httpd -y 
 	
 	logger -s "Installing mod_wsgi"
 	sudo yum install mod_wsgi -y 
 	
-	logger -s "Add mod_wsgi.so to the Apache config"
-	sudo /usr/bin/bash echo "LoadModule wsgi_module modules/mod_wsgi.so" >> /etc/httpd/conf/httpd.conf
-	
-	# System Manager - But you need a whole pile of policy crap - not bothering for now
-	# https://docs.aws.amazon.com/systems-manager/latest/userguide/systems-manager-ug.pdf#systems-manager-quick-setup
-	#sudo yum install -y https://s3.ap-southeast-2.amazonaws.com/amazon-ssm-ap-southeast-2/latest/linux_amd64/amazon-ssm-agent.rpm -y
-	#
 	# Git is already installed, else how did we get here?
 	# sudo yum install git -y   
+		
+	logger -s "Add mod_wsgi.so to the Apache config"
 
+	sudo echo "LoadModule wsgi_module modules/mod_wsgi.so" >> /etc/httpd/conf.d/wsgi.conf
 	
+}
+
+function root_post() {
+	# Steps to run as root after main
+	#dummy homepage
+	
+	echo "<html><body><div>Hello, world\!</div></body></html>" > index.html
+	sudo mv index.html > /var/www/html/index.html
+	
+		
+	logger -s "Enable httpd"
+	sudo systemctl enable httpd
+	
+	logger -s "Start httpd"	
+	sudo systemctl start httpd 
+}
+
+
+function main() {
+
 	# get CTFd
 
 	export APPDIR=${HOME}/app      
-    export TARGET="https://github.com/CTFd/CTFd.git"
+	export TARGET="https://github.com/CTFd/CTFd.git"
 	mkdir -p ${APPDIR}  
-    cd ${APPDIR}	
+	cd ${APPDIR}	
 	logger -s "Clone CTFd from ${TARGET}, save into ${APPDIR}"
 	git clone $TARGET
 	
 	# Create a random key
 	cd CTFd   
-    logger -s "pwd=`pwd`"
-    logger -s "Create .ctfd_secret_key"	
+	logger -s "pwd=`pwd`"
+	logger -s "Create .ctfd_secret_key"	
 	head -c 64 /dev/urandom > .ctfd_secret_key 
+	
+	
 
-	
-	#dummy homepage
-    sudo /usr/bin/bash echo "<html><body><div>Hello, world!</div></body></html>" > /var/www/html/index.html
-	
-		
-	logger -s "Enable httpd"
-    sudo systemctl enable httpd
-	
-    logger -s "Start httpd"	
-    sudo systemctl start httpd 
 }
 
-main 
+case $1 in
+	"pre"*)
+		root_pre
+		;;
+	"post"*)
+		root_post
+		;;
+	"main"*)
+		main
+		;;
+	*)
+		main
+		;;
+esac
+
+echo "Done"
