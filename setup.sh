36fcd33e8e93b1fda81af9fa4edf52df489e58ab
thing-a-verse <michael@wonkie.net>
2021-11-15 15:51:23 +1100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,14 +4,14 @@
 function root_pre() {
 	# Steps to run as root prior to main
 	logger -s "Installing Apache"
-	sudo yum install httpd -y 
-	
+	sudo yum install httpd -y
+
 	logger -s "Installing mod_wsgi"
-	sudo yum install mod_wsgi -y 
-	
+	sudo yum install mod_wsgi -y
+
 	# Git is already installed, else how did we get here?
-	# sudo yum install git -y   
-		
+	# sudo yum install git -y
+
 	logger -s "Add mod_wsgi.so to the Apache config"
 
 	sudo echo "LoadModule wsgi_module modules/mod_wsgi.so" >> /etc/httpd/conf.d/wsgi.conf
@@ -23,12 +23,12 @@ function root_pre() {
 function root_post() {
 	# Steps to run as root after main
 	#dummy homepage
-	
-	echo "<html><body><div>Hello, world</div></body></html>" > /var/www/html/index.html
-	
+	APACHE_LOG_DIR=/var/www/html
+	echo "<html><body><div>If everything works, you shouldn't see this</div></body></html>" > /var/www/html/index.html
+
 	cat <<EOF > /var/www/html/ctf.wsgi
 import sys
-sys.path.insert(0, '/var/www/html')
+sys.path.insert(0, '/var/www/html/ctfd')
 
 from CTFd import create_app
 application = create_app()
@@ -41,8 +41,8 @@ EOF
     DocumentRoot /var/www/html
 
     WSGIScriptAlias / /var/www/html/ctf.wsgi
-    WSGIDaemonProcess ctfd user=ctfd group=ctfd threads=5
-    WSGIProcessGroup ctfd
+    #WSGIDaemonProcess ctfd user=ctfd group=ctfd threads=5
+    #WSGIProcessGroup ctfd
 
     ErrorLog ${APACHE_LOG_DIR}/error.log
     CustomLog ${APACHE_LOG_DIR}/access.log combined
@@ -56,17 +56,17 @@ EOF
 </VirtualHost>
 
 EOF
-	
-	
-	#cd /var/www/html/
-	ln -s /home/ctfd/app/CTFd/ /var/www/html/CTFd
 
-		
+
+	#cd /var/www/html/
+	ln -s /home/ctfd/app/ctfd/ /var/www/html/CTFd/CTFd
+
+
 	logger -s "Enable httpd"
 	sudo systemctl enable httpd
-	
-	logger -s "Start httpd"	
-	sudo systemctl start httpd 
+
+	logger -s "Start httpd"
+	sudo systemctl start httpd
 }
 
 
@@ -74,20 +74,20 @@ function main() {
 
 	# get CTFd
 
-	export APPDIR=${HOME}/app      
+	export APPDIR=${HOME}/app
 	export TARGET="https://github.com/CTFd/CTFd.git"
-	mkdir -p ${APPDIR}  
-	cd ${APPDIR}	
+	mkdir -p ${APPDIR}
+	cd ${APPDIR}
 	logger -s "Clone CTFd from ${TARGET}, save into ${APPDIR}"
 	git clone $TARGET
-	
+
 	# Create a random key
-	cd CTFd   
+	cd CTFd
 	logger -s "pwd=`pwd`"
-	logger -s "Create .ctfd_secret_key"	
-	head -c 64 /dev/urandom > .ctfd_secret_key 
-	
-	
+	logger -s "Create .ctfd_secret_key"
+	head -c 64 /dev/urandom > .ctfd_secret_key
+
+
 
 }
 
