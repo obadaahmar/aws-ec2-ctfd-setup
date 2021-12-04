b9e2315c4eb7ffb01f430621c6bf0d51bc11a150
thing-a-verse <michael@wonkie.net>
2021-12-05 10:15:28 +1100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,18 +17,18 @@ function root_pre() {
 	# Git is already installed, else how did we get here? Well, just in case...
 	headline_logger -s "Installing git"
 	sudo yum install git -y
-	
+
 	#################################################################################################
 	#
 	#  WEB BROWSER - APACHE 2.4
 	#
 	#################################################################################################
-	
+
 
 	headline_logger -s "Installing Apache"
 	sudo yum install httpd -y
-	
-	
+
+
 	#headline_logger -s "Installing mod_wsgi"
 	#sudo yum install mod_wsgi -y                  # No good, this is for python 2
 	#sudo yum install python3-mod_wsgi -y          # No good, this is mod_wsgi v3.4, we want v4.x
@@ -36,55 +36,55 @@ function root_pre() {
 	# Do it the hard way: https://pypi.org/project/mod-wsgi/
 	headline_logger -s "Installing Developer Tools"
 	sudo yum groupinstall "Development Tools" -y   # We need gcc etc ...
-	
+
 	headline_logger -s "Installing Apache Devel"   # We'll need axps (Apache Extension Tool) etc ...
 	sudo yum install httpd-devel -y                # so, we need httpd-devel
 	headline_logger -s "Installing Python Devel"
-	sudo yum install python3-devel.x86_64 -y       # and python-devel 
-	
+	sudo yum install python3-devel.x86_64 -y       # and python-devel
+
 	headline_logger -s "Installing mod wsgi 4.x"
 	curl https://files.pythonhosted.org/packages/b6/54/4359de02da3581ea4a17340d87fd2c5a47adc4c8e626f9809e2697b2d33f/mod_wsgi-4.9.0.tar.gz --output mod_wsgi-4.9.0.tar.gz
 	tar -xzvf mod_wsgi-4.9.0.tar.gz
-	
+
     cd mod_wsgi-4.9.0
-	./configure --with-python=/bin/python3         # we'd like this compiled against python3 thanks 
+	./configure --with-python=/bin/python3         # we'd like this compiled against python3 thanks
 	make
 	sudo make install
-	
-	
+
+
 
     # Apache needs to load mod_wsgi.so in order to run python wsgi
 	logger -s "Add mod_wsgi.so to the Apache config"
 	sudo echo "LoadModule wsgi_module modules/mod_wsgi.so" >> /etc/httpd/conf.d/wsgi.conf
-	
+
 	################################################################################################
 	#
 	#  DATABASE - MARIADB
 	#
 	################################################################################################
-	
+
 	# NOTES - it's possible that the DB isn't initialised properly
-	
+
 	# see vagrantinstall script
-	
-	
+
+
 	# we want to use mariadb10.5 so we need to enable it first as AWS ECS library has v5.x by default
 	# https://aws.amazon.com/premiumsupport/knowledge-center/ec2-install-extras-library-software/
 	# sudo yum install -y amazon-linux-extras
 	headline_logger -s "Enable MariaDB v10.5"
 	sudo amazon-linux-extras enable mariadb10.5
 	sudo yum clean metadata
-	
+
 	headline_logger -s "Installing mysql (MariaDB)"
 	sudo yum install mariadb -y
 	logger -s "Enable MariaDB as a service"
 	sudo systemctl enable mariadb
 	logger -s "Start MariaDB service"
     sudo systemctl start mariadb
-	
+
 	echo "mysql version: `mysql --version`"
     logger -s "Setup and secure Mysql/MariaDB"
-	
+
 	#NB: MariaDB 10 has an extra Yes needed before the password change (remove one if you're using v5.5)
     sudo mysql_secure_installation <<EOF
 
@@ -107,21 +107,21 @@ EOF
     headline_logger -s "Enable redis6"
     sudo amazon-linux-extras enable redis6
     sudo yum clean metadata
-	
+
 	headline_logger -s "Installing redis"
 	sudo yum install redis -y
-	
+
 	CONFIG=/etc/redis/redis.conf
 	# Need to provide a config
 	#  /etc/redis/redis.conf
 	# https://raw.githubusercontent.com/redis/redis/6.2/redis.conf
-	# 
-	# Default config is 
+	#
+	# Default config is
 	#   bind 127.0.0.1 -::1
 	#   protected-mode yes
 	#   port 6379
 	# This is fine, but we'll use an ACL file to specify users
-	
+
 	logger -s "Update the Redis config file $CONFIG: configure REDIS to use ACL file"
 	# Uncomment the aclfile entry
 	sed -i "s|# aclfile /etc/redis/users.acl|aclfile /etc/redis/users.acl|g" $CONFIG
@@ -131,10 +131,10 @@ user ctfd on +@all -DEBUG ~* >secret$SVC
 EOF
 
 	logger -s "Enable redis as a service"
-	sudo systemctl enable redis	
+	sudo systemctl enable redis
 	logger -s "Start redis service"
     sudo systemctl start redis
-	
+
 
 }
 
@@ -149,7 +149,7 @@ function root_post() {
 
     # Write the virtual host to run this site (port 80 - dodgy)
 	logger -s "Write /etc/httpd/conf.d/ctfd.conf"
-	
+
 	cat <<EOF > /etc/httpd/conf.d/ctfd.conf
 <VirtualHost *:80>
     ServerName ctfd.wonkie.cloud
@@ -180,33 +180,33 @@ function root_post() {
 
 EOF
 
-	
+
 	logger -s "Enable httpd as a service"
 	sudo systemctl enable httpd
 	logger -s "Start httpd service"
 	sudo systemctl start httpd
-	
-  
-  
+
+
+
     # Shouldn't leave ggc & dev tools lying around
 	headline_logger -s "Uninstalling Developer Tools"
-	sudo yum erase "Development Tools" -y   
-	
-	headline_logger -s "Uninstalling Apache Devel"   
-	sudo yum erase httpd-devel -y                
-	
+	sudo yum erase "Development Tools" -y
+
+	headline_logger -s "Uninstalling Apache Devel"
+	sudo yum erase httpd-devel -y
+
 }
 
 # SVC commands - run as unpriv user, install main application
 function main() {
     headline_logger -s "Start ${0} installation as `whoami`"
     SVC=${1}
-	
+
     # permit Apache to read from Service Account when running as root
 	logger -s "Ensure httpd can read from Service Account"
 	chmod og+rx ${HOME}
 
-	# get CTFd from GitHub   
+	# get CTFd from GitHub
 	export APPDIR=${HOME}/app
 	export TARGET="https://github.com/CTFd/CTFd.git"
 	mkdir -p ${APPDIR}
@@ -220,9 +220,9 @@ function main() {
 	#logger -s "pwd=`pwd`"
 	#logger -s "Create .ctfd_secret_key in `pwd`"
 	#head -c 64 /dev/urandom > .ctfd_secret_key
-    
+
 	logger -s "Update python modules used by CTFd"
-	
+
 	# Update the Python Modules
 	pip3 install -r requirements.txt
 
@@ -245,20 +245,20 @@ EOF
 	CONFIG=${APPDIR}/CTFd/CTFd/config.ini
 
 
-	# Define the Secret Key	
+	# Define the Secret Key
 
 	logger -s "Update the CTFd config file $CONFIG: configure SECRET_KEY"
 	sed -i "s|# SECRET_KEY =|SECRET_KEY = ${SVC}s3cr3t${SVC}123|g" $CONFIG
-	
-	# Define the Database String	
+
+	# Define the Database String
 	logger -s "Update the CTFd config file $CONFIG: configure DB"
 	sed -i "s|DATABASE_URL =|DATABASE_URL = mysql+pymysql://root:secret$SVC@localhost/ctfd|g" $CONFIG
-	
-	# Define the Cache Server String	
+
+	# Define the Cache Server String
 	logger -s "Update the CTFd config file $CONFIG: configure REDIS"
-	sed -i "s|REDIS_URL =|REDIS_URL = redis://ctfd:secret$SVC@localhost:6379|g" $CONFIG
-	
-	# Define the Application root	
+	#sed -i "s|REDIS_URL =|REDIS_URL = redis://ctfd:secret$SVC@localhost:6379|g" $CONFIG
+
+	# Define the Application root
 	# APPLICATION_ROOT = /home/ctfd/app/CTFd
 	logger -s "Update the CTFd config file $CONFIG: configure APPLICATION_ROOT"
 	sed -i "s|# APPLICATION_ROOT =| APPLICATION_ROOT = / |g" $CONFIG
@@ -268,7 +268,7 @@ EOF
 
 	headline_logger -s "Initialise the DB"
 	python3 manage.py db upgrade
-	
+
 }
 
 SVC=${2:-ctfd}
