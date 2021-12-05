17b46725a38dfa2fa14fa3776099fe0f159253cb
thing-a-verse <michael@wonkie.net>
2021-12-05 19:02:34 +1100

    Tabs to spaces

--- setup.sh
+++ setup.sh
@@ -11,52 +11,52 @@ function headline_logger () {
 # Root commands - pre service account, fetch packages, configure DB & Caching Server
 function root_pre() {
   SVC=${1}
-	headline_logger -s "Start ${0} installation as `whoami`"
-	logger -s "pwd=`pwd`"
+  headline_logger -s "Start ${0} installation as `whoami`"
+  logger -s "pwd=`pwd`"
 
   # Disable SELinux
   CONFIG=/etc/selinux/config
   logger -s "Update the SELinux config file $CONFIG: configure SELINUX=permissive"
-	sed -i "s|SELINUX=enforcing|SELINUX=permissive|g" $CONFIG
+  sed -i "s|SELINUX=enforcing|SELINUX=permissive|g" $CONFIG
   # Disable immediately
   setenforce 0
 
-	# Git is already installed, else how did we get here? Well, just in case...
-	headline_logger -s "Installing git"
-	sudo yum install git -y
+  # Git is already installed, else how did we get here? Well, just in case...
+  headline_logger -s "Installing git"
+  sudo yum install git -y
 
-	#################################################################################################
-	#
-	#  WEB BROWSER - APACHE 2.4
-	#
-	#################################################################################################
-
-
-	headline_logger -s "Installing Apache"
-	sudo yum install httpd -y
+  #################################################################################################
+  #
+  #  WEB BROWSER - APACHE 2.4
+  #
+  #################################################################################################
 
 
-	#headline_logger -s "Installing mod_wsgi"
-	#sudo yum install mod_wsgi -y                  # No good, this is for python 2
-	#sudo yum install python3-mod_wsgi -y          # No good, this is mod_wsgi v3.4, we want v4.x
+  headline_logger -s "Installing Apache"
+  sudo yum install httpd -y
 
-	# Do it the hard way: https://pypi.org/project/mod-wsgi/
-	headline_logger -s "Installing Developer Tools"
-	sudo yum groupinstall "Development Tools" -y   # We need gcc etc ...
 
-	headline_logger -s "Installing Apache Devel"   # We'll need axps (Apache Extension Tool) etc ...
-	sudo yum install httpd-devel -y                # so, we need httpd-devel
-	headline_logger -s "Installing Python Devel"
-	sudo yum install python3-devel.x86_64 -y       # and python-devel
+  #headline_logger -s "Installing mod_wsgi"
+  #sudo yum install mod_wsgi -y                  # No good, this is for python 2
+  #sudo yum install python3-mod_wsgi -y          # No good, this is mod_wsgi v3.4, we want v4.x
 
-	headline_logger -s "Installing mod wsgi 4.x"
-	curl https://files.pythonhosted.org/packages/b6/54/4359de02da3581ea4a17340d87fd2c5a47adc4c8e626f9809e2697b2d33f/mod_wsgi-4.9.0.tar.gz --output mod_wsgi-4.9.0.tar.gz
-	tar -xzvf mod_wsgi-4.9.0.tar.gz
+  # Do it the hard way: https://pypi.org/project/mod-wsgi/
+  headline_logger -s "Installing Developer Tools"
+  sudo yum groupinstall "Development Tools" -y   # We need gcc etc ...
+
+  headline_logger -s "Installing Apache Devel"   # We'll need axps (Apache Extension Tool) etc ...
+  sudo yum install httpd-devel -y                # so, we need httpd-devel
+  headline_logger -s "Installing Python Devel"
+  sudo yum install python3-devel.x86_64 -y       # and python-devel
+
+  headline_logger -s "Installing mod wsgi 4.x"
+  curl https://files.pythonhosted.org/packages/b6/54/4359de02da3581ea4a17340d87fd2c5a47adc4c8e626f9809e2697b2d33f/mod_wsgi-4.9.0.tar.gz --output mod_wsgi-4.9.0.tar.gz
+  tar -xzvf mod_wsgi-4.9.0.tar.gz
 
     cd mod_wsgi-4.9.0
-	./configure --with-python=/bin/python3         # we'd like this compiled against python3 thanks
-	make
-	sudo make install
+  ./configure --with-python=/bin/python3         # we'd like this compiled against python3 thanks
+  make
+  sudo make install
 
   #check diskspace
   echo "Disk space:`df -k`"
@@ -73,40 +73,40 @@ function root_pre() {
 
 
     # Apache needs to load mod_wsgi.so in order to run python wsgi
-	logger -s "Add mod_wsgi.so to the Apache config"
-	sudo echo "LoadModule wsgi_module modules/mod_wsgi.so" >> /etc/httpd/conf.d/wsgi.conf
+  logger -s "Add mod_wsgi.so to the Apache config"
+  sudo echo "LoadModule wsgi_module modules/mod_wsgi.so" >> /etc/httpd/conf.d/wsgi.conf
 
 
 
-	################################################################################################
-	#
-	#  DATABASE - MARIADB
-	#
-	################################################################################################
+  ################################################################################################
+  #
+  #  DATABASE - MARIADB
+  #
+  ################################################################################################
 
-	# NOTES - it's possible that the DB isn't initialised properly
+  # NOTES - it's possible that the DB isn't initialised properly
 
-	# see vagrantinstall script
+  # see vagrantinstall script
 
 
-	# we want to use mariadb10.5 so we need to enable it first as AWS ECS library has v5.x by default
-	# https://aws.amazon.com/premiumsupport/knowledge-center/ec2-install-extras-library-software/
-	# sudo yum install -y amazon-linux-extras
-	headline_logger -s "Enable MariaDB v10.5"
-	sudo amazon-linux-extras enable mariadb10.5
-	sudo yum clean metadata
+  # we want to use mariadb10.5 so we need to enable it first as AWS ECS library has v5.x by default
+  # https://aws.amazon.com/premiumsupport/knowledge-center/ec2-install-extras-library-software/
+  # sudo yum install -y amazon-linux-extras
+  headline_logger -s "Enable MariaDB v10.5"
+  sudo amazon-linux-extras enable mariadb10.5
+  sudo yum clean metadata
 
-	headline_logger -s "Installing mysql (MariaDB)"
-	sudo yum install mariadb -y
-	logger -s "Enable MariaDB as a service"
-	sudo systemctl enable mariadb
-	logger -s "Start MariaDB service"
+  headline_logger -s "Installing mysql (MariaDB)"
+  sudo yum install mariadb -y
+  logger -s "Enable MariaDB as a service"
+  sudo systemctl enable mariadb
+  logger -s "Start MariaDB service"
     sudo systemctl start mariadb
 
-	echo "mysql version: `mysql --version`"
+  echo "mysql version: `mysql --version`"
     logger -s "Setup and secure Mysql/MariaDB"
 
-	#NB: MariaDB 10 has an extra Yes needed before the password change (remove one if you're using v5.5)
+  #NB: MariaDB 10 has an extra Yes needed before the password change (remove one if you're using v5.5)
     sudo mysql_secure_installation <<EOF
 
 y
@@ -119,41 +119,41 @@ y
 y
 EOF
 
-	################################################################################################
-	#
-	#  CACHE - REDIS
-	#
-	################################################################################################
+  ################################################################################################
+  #
+  #  CACHE - REDIS
+  #
+  ################################################################################################
 
     headline_logger -s "Enable redis6"
     sudo amazon-linux-extras enable redis6
     sudo yum clean metadata
 
-	headline_logger -s "Installing redis"
-	sudo yum install redis -y
+  headline_logger -s "Installing redis"
+  sudo yum install redis -y
 
-	CONFIG=/etc/redis/redis.conf
-	# Need to provide a config
-	#  /etc/redis/redis.conf
-	# https://raw.githubusercontent.com/redis/redis/6.2/redis.conf
-	#
-	# Default config is
-	#   bind 127.0.0.1 -::1
-	#   protected-mode yes
-	#   port 6379
-	# This is fine, but we'll use an ACL file to specify users
+  CONFIG=/etc/redis/redis.conf
+  # Need to provide a config
+  #  /etc/redis/redis.conf
+  # https://raw.githubusercontent.com/redis/redis/6.2/redis.conf
+  #
+  # Default config is
+  #   bind 127.0.0.1 -::1
+  #   protected-mode yes
+  #   port 6379
+  # This is fine, but we'll use an ACL file to specify users
 
-	logger -s "Update the Redis config file $CONFIG: configure REDIS to use ACL file"
-	# Uncomment the aclfile entry
-	sed -i "s|# aclfile /etc/redis/users.acl|aclfile /etc/redis/users.acl|g" $CONFIG
-	# Generate a redis.acl file, include use ctfd, permission for all, password as defined
-	cat <<EOF > /etc/redis/users.acl
+  logger -s "Update the Redis config file $CONFIG: configure REDIS to use ACL file"
+  # Uncomment the aclfile entry
+  sed -i "s|# aclfile /etc/redis/users.acl|aclfile /etc/redis/users.acl|g" $CONFIG
+  # Generate a redis.acl file, include use ctfd, permission for all, password as defined
+  cat <<EOF > /etc/redis/users.acl
 user ctfd on +@all -DEBUG ~* >secret$SVC
 EOF
 
-	logger -s "Enable redis as a service"
-	sudo systemctl enable redis
-	logger -s "Start redis service"
+  logger -s "Enable redis as a service"
+  sudo systemctl enable redis
+  logger -s "Start redis service"
     sudo systemctl start redis
 
 
@@ -163,16 +163,16 @@ EOF
 # Root commands - post service account, configure and start apache
 function root_post() {
     SVC=${1}
-	headline_logger -s "Start ${0} installation as `whoami`"
-	# Steps to run as root after main
-	#dummy homepage
-	APACHE_LOG_DIR=/var/log/httpd
-	echo "<html><body><div>If everything works, you shouldn't see this</div></body></html>" > /var/www/html/index.html
+  headline_logger -s "Start ${0} installation as `whoami`"
+  # Steps to run as root after main
+  #dummy homepage
+  APACHE_LOG_DIR=/var/log/httpd
+  echo "<html><body><div>If everything works, you shouldn't see this</div></body></html>" > /var/www/html/index.html
 
     # Write the virtual host to run this site (port 80 - dodgy)
-	logger -s "Write /etc/httpd/conf.d/ctfd.conf"
+  logger -s "Write /etc/httpd/conf.d/ctfd.conf"
 
-	cat <<EOF > /etc/httpd/conf.d/ctfd.conf
+  cat <<EOF > /etc/httpd/conf.d/ctfd.conf
 <VirtualHost *:80>
     ServerName ctfd.wonkie.cloud
 
@@ -182,7 +182,7 @@ function root_post() {
     WSGIScriptAlias / /home/${SVC}/app/ctf.wsgi
     #WSGIDaemonProcess ${SVC} user=${SVC} group=${SVC} processes=5 threads=15 home=/home/${SVC}/app/CTFd
 
-	  WSGIDaemonProcess ${SVC} user=${SVC} group=${SVC} processes=5 threads=15 home=/home/${SVC}/app/CTFd queue-timeout=45 socket-timeout=60 connect-timeout=15 request-timeout=60 inactivity-timeout=0 startup-timeout=15 deadlock-timeout=60 graceful-timeout=15 eviction-timeout=0 restart-interval=0 shutdown-timeout=5 maximum-requests=0
+    WSGIDaemonProcess ${SVC} user=${SVC} group=${SVC} processes=5 threads=15 home=/home/${SVC}/app/CTFd queue-timeout=45 socket-timeout=60 connect-timeout=15 request-timeout=60 inactivity-timeout=0 startup-timeout=15 deadlock-timeout=60 graceful-timeout=15 eviction-timeout=0 restart-interval=0 shutdown-timeout=5 maximum-requests=0
 
 # https://serverfault.com/questions/844761/wsgi-truncated-or-oversized-response-headers-received-from-daemon-process
 # mod_wsgi - some python C extensions are non thread-safe -- WSGIApplicationGroup %{GLOBAL} -- is the fix
@@ -210,10 +210,10 @@ function root_post() {
 EOF
 
 
-	logger -s "Enable httpd as a service"
-	sudo systemctl enable httpd
-	logger -s "Start httpd service"
-	sudo systemctl start httpd
+  logger -s "Enable httpd as a service"
+  sudo systemctl enable httpd
+  logger -s "Start httpd service"
+  sudo systemctl start httpd
 
 
 
@@ -226,34 +226,34 @@ function main() {
     SVC=${1}
 
     # permit Apache to read from Service Account when running as root
-	logger -s "Ensure httpd can read from Service Account"
-	chmod og+rx ${HOME}
+  logger -s "Ensure httpd can read from Service Account"
+  chmod og+rx ${HOME}
 
-	# get CTFd from GitHub
-	export APPDIR=${HOME}/app
-	export TARGET="https://github.com/CTFd/CTFd.git"
-	mkdir -p ${APPDIR}
-	cd ${APPDIR}
-	headline_logger -s "Clone CTFd from ${TARGET}, save into ${APPDIR}"
-	git clone $TARGET
+  # get CTFd from GitHub
+  export APPDIR=${HOME}/app
+  export TARGET="https://github.com/CTFd/CTFd.git"
+  mkdir -p ${APPDIR}
+  cd ${APPDIR}
+  headline_logger -s "Clone CTFd from ${TARGET}, save into ${APPDIR}"
+  git clone $TARGET
 
-	# Create a random key
-	logger -s "Randomise a key"
-	cd CTFd
-	#logger -s "pwd=`pwd`"
-	#logger -s "Create .ctfd_secret_key in `pwd`"
-	#head -c 64 /dev/urandom > .ctfd_secret_key
+  # Create a random key
+  logger -s "Randomise a key"
+  cd CTFd
+  #logger -s "pwd=`pwd`"
+  #logger -s "Create .ctfd_secret_key in `pwd`"
+  #head -c 64 /dev/urandom > .ctfd_secret_key
 
-	logger -s "Update python modules used by CTFd"
+  logger -s "Update python modules used by CTFd"
 
-	# Update the Python Modules
-	pip3 install -r requirements.txt
+  # Update the Python Modules
+  pip3 install -r requirements.txt
 
 
     # Write the .wsgi file than gets executed when you hit the website
-	# Include /usr/local/bin ( why? )
-	logger -s "Configure ctfd.wsgi"
-	cat <<EOF > ${APPDIR}/ctf.wsgi
+  # Include /usr/local/bin ( why? )
+  logger -s "Configure ctfd.wsgi"
+  cat <<EOF > ${APPDIR}/ctf.wsgi
 import sys
 sys.path.insert(0, '${APPDIR}/CTFd')
 
@@ -278,32 +278,32 @@ EOF
 
     logger -s "Update the CTFd config file"
     # Update the config file
-	CONFIG=${APPDIR}/CTFd/CTFd/config.ini
+  CONFIG=${APPDIR}/CTFd/CTFd/config.ini
 
 
-	# Define the Secret Key
+  # Define the Secret Key
 
-	logger -s "Update the CTFd config file $CONFIG: configure SECRET_KEY"
-	sed -i "s|# SECRET_KEY =|SECRET_KEY = ${SVC}s3cr3t${SVC}123|g" $CONFIG
+  logger -s "Update the CTFd config file $CONFIG: configure SECRET_KEY"
+  sed -i "s|# SECRET_KEY =|SECRET_KEY = ${SVC}s3cr3t${SVC}123|g" $CONFIG
 
-	# Define the Database String
-	logger -s "Update the CTFd config file $CONFIG: configure DB"
-	sed -i "s|DATABASE_URL =|DATABASE_URL = mysql+pymysql://root:secret$SVC@localhost/ctfd|g" $CONFIG
+  # Define the Database String
+  logger -s "Update the CTFd config file $CONFIG: configure DB"
+  sed -i "s|DATABASE_URL =|DATABASE_URL = mysql+pymysql://root:secret$SVC@localhost/ctfd|g" $CONFIG
 
-	# Define the Cache Server String
-	logger -s "Update the CTFd config file $CONFIG: configure REDIS"
-	sed -i "s|REDIS_URL =|REDIS_URL = redis://ctfd:secret$SVC@localhost:6379|g" $CONFIG
+  # Define the Cache Server String
+  logger -s "Update the CTFd config file $CONFIG: configure REDIS"
+  sed -i "s|REDIS_URL =|REDIS_URL = redis://ctfd:secret$SVC@localhost:6379|g" $CONFIG
 
-	# Define the Application root
-	# APPLICATION_ROOT = /home/ctfd/app/CTFd
-	logger -s "Update the CTFd config file $CONFIG: configure APPLICATION_ROOT"
-	sed -i "s|# APPLICATION_ROOT =| APPLICATION_ROOT = / |g" $CONFIG
+  # Define the Application root
+  # APPLICATION_ROOT = /home/ctfd/app/CTFd
+  logger -s "Update the CTFd config file $CONFIG: configure APPLICATION_ROOT"
+  sed -i "s|# APPLICATION_ROOT =| APPLICATION_ROOT = / |g" $CONFIG
 
-	headline_logger -s "Check the DB is available"
-	python3 ping.py
+  headline_logger -s "Check the DB is available"
+  python3 ping.py
 
-	headline_logger -s "Initialise the DB"
-	python3 manage.py db upgrade
+  headline_logger -s "Initialise the DB"
+  python3 manage.py db upgrade
 
 
 
@@ -313,18 +313,18 @@ EOF
 SVC=${2:-ctfd}
 headline_logger -s "setup.sh: $1 $2 (SVC=${SVC})"
 case $1 in
-	"pre"*)
-		root_pre $SVC
-		;;
-	"post"*)
-		root_post $SVC
-		;;
-	"main"*)
-		main $SVC
-		;;
-	*)
-		main
-		;;
+  "pre"*)
+  root_pre $SVC
+  ;;
+  "post"*)
+  root_post $SVC
+  ;;
+  "main"*)
+  main $SVC
+  ;;
+  *)
+  main
+  ;;
 esac
 headline_logger -s "setup.sh: done"
 echo "Done"
